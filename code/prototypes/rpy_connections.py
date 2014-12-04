d80d99e1f2d17553d954b084ca7f744e83b46797
karenyyng <karen.yyng@gmail.com>
2014-12-03 21:30:54 -0800

    got more r interfacing code working

--- code/prototypes/rpy_connections.py
+++ code/prototypes/rpy_connections.py
@@ -21,34 +21,56 @@ def convert_fhat_to_dict(r_fhat):
     :stability: works but should be tested
     """
 
-    return {"x": np.array(r_fhat[0]).transpose(),
-            "eval.points": np.array(r_fhat[1]),
+    return {"data_x": np.array(r_fhat[0]).transpose(),
+            "eval_points": np.array(r_fhat[1]),
             "estimate": np.array(r_fhat[2]),
-            "H": np.array(r_fhat[3]),
+            "bandwidth_matrix_H": np.array(r_fhat[3]),
             "gridtype": tuple(r_fhat[4]),
             "gridded": bool(r_fhat[5]),
             "binned": bool(r_fhat[6]),
             "names": list(r_fhat[7]),
-            "w": np.array(r_fhat[8])}
+            "weight_w": np.array(r_fhat[8])}
 
 
-def do_KDE(data, bandwidth_selector, w=None):
-    func = robjects.r["do_KDE"]
+def do_KDE(data, bandwidth_selector, w=None, verbose=False):
+    doKDE = robjects.r["do_KDE"]
 
     if w is not None:
-        return convert_fhat_to_dict(func(data, bandwidth_selector, w))
+        fhat = doKDE(data, bandwidth_selector, w)
     else:
-        return convert_fhat_to_dict(func(data, bandwidth_selector))
+        fhat = doKDE(data, bandwidth_selector)
+
+    return fhat
+
+
+def get_peaks(fhat):
+    """
+    fhat = robject spat out from ks.KDE
+    """
+    findPeaks = robjects.r["find_peaks_from_2nd_deriv"]
+    findDomPeaks = robjects.r["find_dominant_peaks"]
+
+    peaks_coords = findPeaks(fhat[2])  # fhat[2] = fhat$estimate
+    dom_peaks = np.array(findDomPeaks(fhat, peaks_coords))
+
+    fhat = convert_fhat_to_dict(fhat)
+    fhat["peaks"] = np.array(peaks_coords)
+    fhat["domPeaks"] = dom_peaks
+
+    return fhat
 
 
 def TwoDtestCase1(samp_no=5e2, cwt=1. / 11., w=None, H=None):
     """call the TwoDtestCase1 for getting data with 3 Gaussian mixtures
     """
     func = robjects.r["TwoDtestCase1"]
+
     if w is not None:
-        return convert_fhat_to_dict(func(samp_no, cwt, w))
+        fhat = func(samp_no, cwt, w)
     else:
-        return convert_fhat_to_dict(func(samp_no, cwt))
+        fhat = func(samp_no, cwt)
+
+    return get_peaks(fhat)
 
 
 def rmvnorm_mixt(n, mus, Sigmas, props):
@@ -61,8 +83,13 @@ def rmvnorm_mixt(n, mus, Sigmas, props):
     x = robject, more specifically, R vector,
         that contains coordinates of the normal mixture
     """
+    # need to import a library
+    robjects.r[""]
 
     return
 
 
+def find_peaks_from_2nd_deriv(dens, verbose=False):
+    func = robjects.r["find_peaks_from_2nd_deriv"]
 
+    return
