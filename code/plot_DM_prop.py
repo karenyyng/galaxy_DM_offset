b8d81fa2854c39161cdf9d22f099f8c6368dc92d
karenyyng <karen.yyng@gmail.com>
2015-05-26 13:51:07 -0700

    good histogram for visualization made for particle data

--- code/plot_DM_prop.py
+++ code/plot_DM_prop.py
@@ -2,21 +2,35 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def plot_DM_fhat(fhat, clstNo, peaks_mask):
-    plt.figure(figsize=(20, 20))
-    plt.axes().set_aspect('equal')
-    plt.title("Clst {}".format(clstNo), size=20)
-    plt.xlabel("kpc", size=30)
-    plt.ylabel("kpc", size=30)
-    plt.tick_params(labelsize='large')
+def plot_DM_fhat(fhat, fhat_stars,  clstNo, threshold=0.3):
+    peaks_mask = fhat["peaks_dens"] > threshold
 
+    if "log_est" not in fhat.keys():
+        fhat["log_est"] = np.log(fhat["estimate"])
+
+    plt.subplot('111', axisbg='black', aspect='equal')
+
+    plt.tick_params(axis='both', which='both', labelsize=30)
     plt.contourf(fhat["eval_points"][0], fhat["eval_points"][1],
-                 fhat["estimate"].transpose(), cmap=plt.cm.afmhot)
+                 fhat["log_est"].transpose(), cmap=plt.cm.afmhot)
 
     ixes = np.arange(0, len(fhat["peaks_xcoords"]), 1)[peaks_mask]
     plt.plot(fhat["peaks_xcoords"][ixes], fhat["peaks_ycoords"][ixes],
-             "ro", fillstyle="none", mew=1.5, ms=20,
-             label="identified peaks")
+             "o", color='cyan', fillstyle="none", mew=1.5,
+             ms=25, label="DM peaks")
 
-    plt.legend(loc='best', frameon=True, fancybox=True)
+    plt.plot(fhat_stars["peaks_xcoords"],
+             fhat_stars["peaks_ycoords"],
+             'o', color='red', fillstyle="none", mew=3, ms=25,
+             label="I band luminosity peaks")
+
+    plt.title('Cluster {0}: DM peak density threshold = {1}'.format(clstNo,
+                                                                    threshold))
+
+    lgd = plt.legend(loc='best', fontsize=30, frameon=1)
+    frame = lgd.get_frame()
+    frame.set_facecolor('white')
+    plt.xlabel('kpc', size=30)
+    plt.ylabel('kpc', size=30)
+
     return
