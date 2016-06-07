f8113a7fd0aa6b46f079c83ee852ca91a84c3295
karenyyng <karen.yyng@gmail.com>
2016-06-07 14:45:28 -0700

    Add function to compute relaxedness for final results

--- code/compute_clst_prop.py
+++ code/compute_clst_prop.py
@@ -16,14 +16,43 @@ def subhalo_cent_dist(df):
 
 
 def compute_relaxedness0(df, f, clstNo):
+    """
+    when fraction_of_Subf_subhalo_mass / M_FOF < 0.1
+    the cluster is considered as relaxed.
+    :param df: pandas dataframe, each row of the dataframe is extracted by
+        extract_catalog.extract_clst(f, clstNo)
+    :param f: hdf5 file stream of the main illustris fof data file
+    :param clstNo: integer, denotes the cluster no with 0-based indexing
+    """
     groupM = f["Group"]["GroupMass"][clstNo]
     subhaloM_total = np.sum(df["SubhaloMass"][1:])
 
     return subhaloM_total / groupM * 100.
 
 
-def compute_non_relaxedness1():
-    return
+def compute_non_relaxedness1(df, f, clstNo):
+    """
+    when relative_dist_{CM-most_bound} = dist(CM, most_bound) / R200C < 0.07
+    the cluster is considered as relaxed.
+
+    implementation notes:
+        have to calculate the center of mass from subfind subhalos
+        or we can use the weighted centroid as a substitute?
+
+    :param df: pandas dataframe
+        contains all the subhalo info of this cluster
+    :param f: hdf5 file stream of the main hdf5 file
+    :param clstNo: integer
+    """
+    center_of_mass = np.array([
+        np.sum(df['SubhaloPos{i}'.format(i)] * df['SubhaloMass'])
+                / np.sum(df['SubhaloMass'])
+        for i in range (3)])
+
+    dist_CM = np.sqrt(np.dot(center_of_mass, center_of_mass))
+    R200C = f["Group/Group_R_Crit200"][clstNo]
+
+    return dist_CM / R200C
 
 
 # def compute_relaxedness1(df, f, clstNo):
