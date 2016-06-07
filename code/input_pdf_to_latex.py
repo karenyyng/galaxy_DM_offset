ebeaa11b35d707410f9672841cd735d0bfcd55c4
karenyyng <karenyng@ucdavis.edu>
2016-06-07 11:43:04 -0700

    Fix styles for tables

--- code/input_pdf_to_latex.py
+++ code/input_pdf_to_latex.py
@@ -15,7 +15,7 @@ from plot_cred_int import round_to_n_sig_fig
 
 def compile_data_lines(df, df_keys, row_labels, n_sig_fig=4):
     return [
-        "{0} & {1} & {2} - {3} & {4} - {5} \\\\".format(
+        "{0} & {1} & {2} ,  {3} & {4} , {5} \\\\".format(
             row_labels[i],
             round_to_n_sig_fig(df.ix[key, "loc"], n_sig_fig),
             round_to_n_sig_fig(df.ix[key, "low68"], n_sig_fig),
@@ -28,17 +28,17 @@ def compile_data_lines(df, df_keys, row_labels, n_sig_fig=4):
 
 def compile_line_list(df, df_keys, row_labels, table_properties):
     line_lists = [
-        "\setcounter{table}{0}",
         "\\begin{table}",
         "\caption{" + table_properties['title'] + "}",
         "\\begin{center}",
         "\\begin{tabular}{@{}lcccc}\n",
-        "\hline \hline Offset & Location & 68% CI & 95% CI \\\\ \hline\n",
+        "\hline \hline Offset & Location & 68\% CI$^\\dagger$ & 95\% CI$^\\dagger$ \\\\ \hline\n",
     ] + compile_data_lines(df, df_keys, row_labels) + [
         "\hline",
         "\end{tabular} ",
         "\end{center} ",
         "\label{tab:" + "{0}".format(table_properties['shortcut_label']) + "}",
+        "\\footnotesize{$\dagger$ CI stands for confidence interval.}\\\\",
         "\end{table}"
     ]
 
@@ -54,14 +54,14 @@ if __name__ == "__main__":
     F = open(paper2 + "input_table.tex", "w")
 
     full_sample_tab_prop = {
-        "title": "stuff",
-        "shortcut_label": "stuff",
+        "title": "Offsets for the full sample of 43 clusters at 2 kpc resolution.",
+        "shortcut_label": "full2kpc_offsets",
     }
 
     full_sample_abs_tab_prop = {
         "title":
-        "Full sample",
-        "shortcut_label": "stuff",
+        "Absolute offsets for the full sample of 43 clusters at 2 kpc resolution.",
+        "shortcut_label": "full_2kpc_abs_offsets",
     }
 
     df = pd.read_hdf(data_path + h5_file, key="df")
@@ -81,7 +81,7 @@ if __name__ == "__main__":
     ]
 
     lines = compile_line_list(df, df_keys, row_labels, full_sample_tab_prop)
-    abs_lines = compile_line_list(df, df_keys, row_labels,
+    abs_lines = compile_line_list(abs_df, df_keys, row_labels,
                                   full_sample_abs_tab_prop)
     map(print, lines)
     map(print, abs_lines)
